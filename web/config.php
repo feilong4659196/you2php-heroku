232103c1422c596d5662a5c70bbc9cf752594080
feilong4659196 <45815357+feilong4659196@users.noreply.github.com>
2019-01-17 11:18:40 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,8 +3,8 @@ $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyC2mp6eD1vucTgiH5lbkr5WcE8mZuHe6Fo');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'JJ5CM');
-define('TITLENAME', 'JJ5CM');
+define('SITE_NAME', 'feilong');
+define('TITLENAME', 'feilong');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'justsweetpotato@gmail.com');
 ?>
